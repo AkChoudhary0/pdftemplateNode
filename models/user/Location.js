2a5178b60629af742fc9045d38b23cbc75153419
Anil Chaudhary <choudhary22anil@gmail.com>
2025-10-07 16:57:40 +0530

    check data

--- models/user/Location.js
+++ models/user/Location.js
@@ -5,11 +5,11 @@ const locationSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
     tag: { type: String },
-    description: [{ type: String }],
+    description: {type:[]},
     note: { type: String },
     img: { type: String },
+    img1: { type: String },
     img2: { type: String },
-    img3: { type: String },
     SIC_price: { type: Number },
     PVT_price: { type: Number },
     transfer_price: { type: Number },
